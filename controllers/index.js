45dbb0b21af6d766c0d478c1f6815c62918011d7
rebecca <fulkerson.rebecca@gmail.com>
2022-10-24 17:20:49 -0400

    added controller to get recipe by Id; tested successfully in insomnia

--- controllers/index.js
+++ controllers/index.js
@@ -15,6 +15,28 @@ const createRecipe = async (req, res) => {
 
 }
 
+const getAllRecipes = async (req, res) => {
+    try {
+        const recipes = await Recipe.find()
+        return res.status(200).json({recipes})
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+const getRecipeById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const recipe = await Recipe.findById(id)
+        if (recipe) {
+            return res.status(200).json({recipe})
+        }
+        return res.status(404).send('Recipe with specified ID does not exist')
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
 const createType = async (req, res) => {
     try {
         const type = await new FoodType(req.body)
@@ -30,4 +52,6 @@ const createType = async (req, res) => {
 module.exports = {
     createRecipe,
     createType,
+    getAllRecipes,
+    getRecipeById
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ const controllers = require('../controllers')
 router.get('/', (req, res) => res.send('This is root!'))
 router.post('/recipes', controllers.createRecipe)
 router.post('/recipetypes', controllers.createType)
+router.get('/allrecipes', controllers.getAllRecipes)
+router.get('/recipes/:id', controllers.getRecipeById)
 
 module.exports = router;
